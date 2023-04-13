42b61a6db62c7dd96b5f0af1a93eb8d2fdfaec9a
Erik Na <erikgnaa@gmail.com>
2023-04-12 23:47:30 -0300

    add: almost finished code

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,6 @@
 import GameBoard from "./views/GameBoard.vue";
 import Dashboard from "./views/Dashboard.vue";
-import { createWebHashHistory, createRouter } from "vue-router"
+import { createMemoryHistory, createRouter } from "vue-router"
 
 const routes = [
     { path: "/", component: GameBoard },
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createMemoryHistory(),
     routes,
 })
 
