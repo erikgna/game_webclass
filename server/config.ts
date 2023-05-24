4bd89b60c691f8e1c189adaf2f868cfbc9bdad65
Erik Na <erikgnaa@gmail.com>
2023-05-24 00:10:03 -0300

    traefik

--- server/config.ts
+++ server/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = 8030;
-export const CORS = "http://web_game_class:8029";
+export const CORS = "https://webclass.erikna.com";
 export const MONGO_CONNECTION =
   "mongodb://erik:Utc30rgaq*@mongo_web_class_api:27018/";
 export const REDIS_CONNECTION =
