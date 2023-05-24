07fa6d67dfb5bce79072762df3dc1254a20d1e52
Erik Na <erikgnaa@gmail.com>
2023-05-23 23:59:07 -0300

    traefik

--- frontend/src/views/GameBoard/GameBoard.vue
+++ frontend/src/views/GameBoard/GameBoard.vue
@@ -5,7 +5,8 @@ import { GameStatus, SocketConstants } from "../../constants/Socket.constants";
 </script>
 
 <script lang="ts">
-const socket = io(import.meta.env.VITE_SOCKET_URL ?? "");
+// const socket = io(import.meta.env.VITE_SOCKET_URL ?? "");
+const socket = io("https://webclassapi.erikna.com");
 const lines = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
