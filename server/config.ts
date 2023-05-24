cc072a0eedffcb01a8cedf1d2cd553b76d1a439e
Erik Na <erikgnaa@gmail.com>
2023-05-24 00:19:41 -0300

    traefik

--- server/config.ts
+++ server/config.ts
@@ -6,6 +6,5 @@ export const PORT = 8030;
 export const CORS = "https://webclass.erikna.com";
 export const MONGO_CONNECTION =
   "mongodb://erik:Utc30rgaq*@mongo_web_class_api:27018/";
-export const REDIS_CONNECTION =
-  "mongodb://erik:Utc30rgaq*@mongo_web_class_api:27018/";
+export const REDIS_CONNECTION = "redis://redis_web_class_api:6020";
 export const REDIS_PASSWORD = "Utc30rgaq*";
